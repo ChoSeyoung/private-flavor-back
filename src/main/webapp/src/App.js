bd81b0d79d8cf7ba037c2c9a720e41e2d58e3914
ChoSeyoung <a01075511157@gmail.com>
2022-06-21 00:04:44 +0900

    스타일 설정

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -1,8 +1,21 @@
-import Header from './components/common/Header';
+import {Col, Container, Row} from "react-bootstrap";
 
 function App() {
   return (
-    <Header />
+      <Container fluid>
+        <Row className="header">
+          <Col xs={2}>
+            <img src="http://placeimg.com/100/50/people"/>
+          </Col>
+          <Col>
+              <FontAwesomeIcon icon="fa-solid fa-folder" />
+          </Col>
+        </Row>
+        <Row>
+          <col>
+          </col>
+        </Row>
+      </Container>
   );
 }
 
--- src/main/webapp/src/components/common/Header.js
+++
@@ -1,9 +0,0 @@
-const header = () => {
-    return (
-      <div>
-          <h1>hello i am header</h1>
-      </div>
-    );
-}
-
-export default header;
