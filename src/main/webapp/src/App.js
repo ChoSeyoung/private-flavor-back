492374bcb1bdd99ffaf36029db4421956d49335a
ChoSeyoung <a01075511157@gmail.com>
2022-06-27 21:26:05 +0900

    마이그레이션 쿼리 작성 중

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -15,31 +15,41 @@ function App() {
                     <Row>
                         <Col>
                             <div className="sidebar-parent">1st flavor</div>
-                            <div className="sidebar-child">1-1st flavor</div>
-                            <div className="sidebar-child">1-2nd flavor</div>
-                            <div className="sidebar-child">1-3rd flavor</div>
+                            <div>
+                                <div className="sidebar-child selected">1-1st flavor</div>
+                                <div className="sidebar-child">1-2nd flavor</div>
+                                <div className="sidebar-child">1-3rd flavor</div>
+                            </div>
                             <div className="sidebar-parent">2nd flavor</div>
-                            <div className="sidebar-child">2-1st flavor</div>
-                            <div className="sidebar-child">2-2nd flavor</div>
-                            <div className="sidebar-child">2-3rd flavor</div>
+                            <div>
+                                <div className="sidebar-child">2-1st flavor</div>
+                                <div className="sidebar-child">2-2nd flavor</div>
+                                <div className="sidebar-child">2-3rd flavor</div>
+                            </div>
                             <div className="sidebar-parent">3rd flavor</div>
-                            <div className="sidebar-child">3-1st flavor</div>
-                            <div className="sidebar-child">3-2nd flavor</div>
-                            <div className="sidebar-child">3-3rd flavor</div>
+                            <div>
+                                <div className="sidebar-child">3-1st flavor</div>
+                                <div className="sidebar-child">3-2nd flavor</div>
+                                <div className="sidebar-child">3-3rd flavor</div>
+                            </div>
                             <div className="sidebar-parent">4th flavor</div>
-                            <div className="sidebar-child">4-1st flavor</div>
-                            <div className="sidebar-child">4-2nd flavor</div>
-                            <div className="sidebar-child">4-3rd flavor</div>
+                            <div>
+                                <div className="sidebar-child">4-1st flavor</div>
+                                <div className="sidebar-child">4-2nd flavor</div>
+                                <div className="sidebar-child">4-3rd flavor</div>
+                            </div>
                             <div className="sidebar-parent">5th flavor</div>
-                            <div className="sidebar-child">5-1st flavor</div>
-                            <div className="sidebar-child">5-2nd flavor</div>
-                            <div className="sidebar-child">5-3rd flavor</div>
+                            <div>
+                                <div className="sidebar-child">5-1st flavor</div>
+                                <div className="sidebar-child">5-2nd flavor</div>
+                                <div className="sidebar-child">5-3rd flavor</div>
+                            </div>
                         </Col>
                     </Row>
                 </Col>
                 <Col>
                     <Row className="align-items-center row-top">
-                        <Col xs={8} className="text-start" >
+                        <Col xs={8} className="text-start">
                             <FontAwesomeIcon icon={faBars}/>
                         </Col>
                         <Col xs={2} className="text-end">
